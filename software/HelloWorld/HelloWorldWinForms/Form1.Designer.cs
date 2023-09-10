795f40bab59ffe1c22d3c6da2867ab4a348f73b6
Devorag <141979085+Devorag@users.noreply.github.com>
2023-09-10 17:11:50 -0400

    Hello World fill in forms

---
+++ software/HelloWorld/HelloWorldWinForms/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace HelloWorldWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(367, 601);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(432, 52);
+            button1.TabIndex = 0;
+            button1.Text = "click me to say hello";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.FromArgb(255, 192, 192);
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Location = new Point(26, 280);
+            label1.Name = "label1";
+            label1.Size = new Size(1135, 81);
+            label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(15F, 38F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1227, 684);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "DG Hello World";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+    }
+}
