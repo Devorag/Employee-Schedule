69ba57eeef898706f7192101ce61122aeb1e05ea
Devorag <141979085+Devorag@users.noreply.github.com>
2023-09-18 13:27:56 -0400

    #3 fixed mistakes

--- Sessions/03/HW.sql
+++ Sessions/03/HW.sql
@@ -10,7 +10,8 @@ Show the medalist that is suing the Olympic Committee.
 -- SM -10% Formatting tip: Each clause should be on new line.
 SELECT * 
 from Medalist m 
-where m.country = 'France' and m.code like '_s%'
+where m.country = 'France' 
+and m.code like '_s%'
 
 /* 2 
 In the year 2000 a debate rose up among the Medalists of Scandinavia (Norway, Sweden, Finland and Denmark). 
@@ -24,18 +25,22 @@ For B) Use only one select statement that will show all this info (you will need
 -- SM -10% Formatting tip: Each clause should be on new line.
 SELECT m.Country, m.OlympicYear 
 from medalist m 
-where m.country in ('Norway' , 'Sweden', 'Finland' , 'Denmark') and m.OlympicYear BETWEEN 1900 and 2000 
+where m.country in ('Norway' , 'Sweden', 'Finland' , 'Denmark') 
+and m.OlympicYear BETWEEN 1900 and 2000 
 order by m.Country, m.OlympicYear
 //2
 -- SM -10% For 1913+ Only include Norway.
 SELECT m.OlympicYear, m.country, m.season 
 from Medalist m 
-where m.Country in ('Sweden' , 'Norway')
-    and (
-    m.OlympicYear BETWEEN 1965 and 1975 
-    or (
-    m.OlympicYear >= 1913 and m.Season = 'summer'
-))
+where (
+    m.Country = 'Sweden'
+    and m.OlympicYear BETWEEN 1965 and 1975 
+)
+    or 
+(
+    m.Country = 'Norway'
+    and m.OlympicYear >= 1913 and m.Season = 'summer'
+)
 order by m.olympicYear, m.Country, m.Season
 
 
@@ -49,13 +54,14 @@ Show columns: First Name, Last Name, Country, Olympic Location, Season. Sort by 
 -- SM -100% This returns error. Formatting tip: Each clause should be on new line.
 SELECT m.FirstName, m.LastName, m.Country, m.OlympicLocation, m.Season 
 FROM MEDALIST M 
-where m.season = winter and m.OlympicLocation like '%' + m.country + '%' 
+where m.season = 'winter' 
+and m.OlympicLocation like '%' + m.country + '%' 
 order by m.Country, m.OlympicLocation
 
 -- SM -100% This returns error.
 SELECT m.FirstName, m.LastName, m.Country, m.OlympicLocation, m.Season 
 FROM MEDALIST M 
-where m.season = winter
+where m.season = 'winter'
 and m.OlympicLocation not like '%' + m.Country + '%'
 order by m.Country, m.OlympicLocation
 
@@ -69,14 +75,19 @@ Show the data that would support or disprove their claim, in one result set. Sho
 -- SM -50% For 1932 show all medals. For 1910-1930 show only gold or silver.
 SELECT m.OlympicYear, m.SportSubcategory, m.medal
 from Medalist m 
-where m.Medal in ('gold' , 'silver')
-    and (
-    m.OlympicYear BETWEEN 1910 and 1930 
-    or m.OlympicYear = 1932
+where 
+(
+    (m.Medal = 'gold' 
+    or m.Medal = 'silver')
+    and m.OlympicYear BETWEEN 1910 and 1930 
+    and (m.SportSubcategory like '%ladies%' 
+    or m.SportSubcategory like '%wom%')
 )
-    AND (
-    m.SportSubcategory like '%ladies%' 
-    or m.SportSubcategory like '%wom%'
+    or
+(
+    m.OlympicYear = 1932
+    and (m.SportSubcategory like '%ladies%' 
+    or m.SportSubcategory like '%wom%')
 )
 
 
@@ -104,7 +115,7 @@ where m.SportSubcategory like '%s'''
 select *
 from Medalist M
 where m.SportSubcategory like '%s''%'
-
+and m.SportSubcategory not like '%s'''
 
 
 
