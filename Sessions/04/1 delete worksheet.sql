997109c0f62723d87cef84d41847e46fd2d48750
Devorag <141979085+Devorag@users.noreply.github.com>
2023-09-18 00:13:18 -0400

    #4

--- Sessions/04/1 delete worksheet.sql
+++ Sessions/04/1 delete worksheet.sql
@@ -1,9 +1,22 @@
 --WorldRecord, re-insert rows after each delete
 --1. Delete all rows in the table, using the shorthand syntax
+delete WorldRecord
 
+select * from WorldRecord
 --2. Delete all rows using the longhand syntax
+delete from worldrecord
 
+delete w from WorldRecord 
 --3. Delete all where record is concerning a dog (the kind that barks). First do as select, then convert to delete
+delete w 
+SELECT * 
+from WorldRecord w 
+where w.RecordDesc like '%dog%' 
+and w.Category = 'animal'
 
 --4 Delete all that were achieved before 2005 or after 2014, and were from USA 
-
+delete w 
+--SELECT *
+from WorldRecord w 
+where w.YearAchieved < 2005 or w.YearAchieved > 2014
+and w.Country = 'USA'
--- Sessions/04/HW.sql
+++ Sessions/04/HW.sql
@@ -1,15 +1,27 @@
 --For all questions do long-hand delete unless otherwise specified, first do as select and then delete, after each question re-insert the data
 
 --1 Delete all medalist records using shorthand syntax
-
+delete Medalist
 --2 Delete all medalist records using longhand syntax
-
+DELETE m from Medalist m
 --3 In medalist delete all where age of medalist is less than 18 or over 60, in your select statement show the age
-
+DELETE m
+--SELECT age = m.OlympicYear - m.YearBorn, *
+from Medalist m 
+where m.OlympicYear - m.YearBorn < 18 or m.OlympicYear - m.yearborn > 60
 --4 In medalist delete all medalists with two last names, with or without a hyphen (Smith Jones or Smith-Jones). Only show the first and last name in your select statement. 
-
+DELETE m
+--SELECT m.FirstName, m.LastName
+from Medalist m 
+where m.LastName like '% %' or m.LastName like '%-%'
 /*5 
 In invention delete all records where it is impossible that this inventor created this invention (based on year born or died). 
 In select statement show columns in this order Invention Name, Inventor Last Name, Year Invented, Year Born, Year Died, Age at time of invention
 */
+DELETE i 
+--SELECT i.InventionName, i.InventorLastName, i.YearInvented, i.YearBorn, i.YearDied, age= i.YearInvented - i.YearBorn
+from Invention i
+where i.YearDied < i.YearInvented
+or i.YearBorn > i.YearInvented
+
 
