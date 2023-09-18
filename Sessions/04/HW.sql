f60d36174b5159dff124b5d41a567ac5d9ac741b
shmielcpu <sminz@cpucodeschool.com>
2023-09-18 15:47:07 +0000

    #4 HW mark

--- Sessions/04/HW.sql
+++ Sessions/04/HW.sql
@@ -1,3 +1,4 @@
+-- SM Excellent! 100% See comments, no need to resubmit.
 --For all questions do long-hand delete unless otherwise specified, first do as select and then delete, after each question re-insert the data
 
 --1 Delete all medalist records using shorthand syntax
@@ -5,11 +6,13 @@ delete Medalist
 --2 Delete all medalist records using longhand syntax
 DELETE m from Medalist m
 --3 In medalist delete all where age of medalist is less than 18 or over 60, in your select statement show the age
+-- SM Tip: Can use "not between"
 DELETE m
 --SELECT age = m.OlympicYear - m.YearBorn, *
 from Medalist m 
 where m.OlympicYear - m.YearBorn < 18 or m.OlympicYear - m.yearborn > 60
 --4 In medalist delete all medalists with two last names, with or without a hyphen (Smith Jones or Smith-Jones). Only show the first and last name in your select statement. 
+-- SM Tip: Can use like with []
 DELETE m
 --SELECT m.FirstName, m.LastName
 from Medalist m 
@@ -18,6 +21,7 @@ where m.LastName like '% %' or m.LastName like '%-%'
 In invention delete all records where it is impossible that this inventor created this invention (based on year born or died). 
 In select statement show columns in this order Invention Name, Inventor Last Name, Year Invented, Year Born, Year Died, Age at time of invention
 */
+-- SM Tip: Can use "not between"
 DELETE i 
 --SELECT i.InventionName, i.InventorLastName, i.YearInvented, i.YearBorn, i.YearDied, age= i.YearInvented - i.YearBorn
 from Invention i
