a0b6b5697079f44c8f12780a8367b9c150e1af38
shmielcpu <sminz@cpucodeschool.com>
2023-11-07 16:23:43 +0000

    #16 HW comments

--- Databases/RecordKeeper/1 table presidents.sql
+++ Databases/RecordKeeper/1 table presidents.sql
@@ -1,3 +1,5 @@
+-- SM Excellent! See comments, fix and resubmit.
+
 /*President
     The U.S Government used the data in the presidents table to provide the captions for new monuments engraved into the side of a mountain in NY.
     There was bad data in the table, some term ends and term starts were reversed. 
@@ -16,14 +18,20 @@ drop table if exists president
 go
 		create table dbo.president(
 		PresidentId int not null identity (1000,1) primary key,
+-- SM Should be > 0.
 		Num int not null CONSTRAINT u_president_Num unique, 
+-- SM Don't allow blank.
 		FirstName varchar(100) not null, 
+-- SM Don't allow blank.
 		LastName varchar(100) not null, 
+-- SM Don't allow blank.
         Party varchar(50) not null,
+-- SM Don't allow future date.
 		DateBorn date not null,
 		DateDied datetime2,
 		TermStart int not null constraint ck_president_term_Start_cannot_Be_Before_1776 check(TermStart >= 1776),
 		TermEnd int,
+-- SM Also ensure the president is alive durring his full term. You might need to update data file.
 		CONSTRAINT ck_president_term_end_Cannot_be_before_term_start CHECK(TermEnd >= TermStart),
 		constraint ck_president_must_be_at_least_35_years_old check(TermStart - year(dateborn) >= 35)
 	)
