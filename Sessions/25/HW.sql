25a1700435a994fafd3882584bb67794ce53848a
shmielcpu <sminz@cpucodeschool.com>
2024-01-11 15:15:34 +0000

    #27 HW comments

--- Sessions/25/HW.sql
+++ Sessions/25/HW.sql
@@ -1,3 +1,4 @@
+-- SM Excellent! See comments, fix and resubmit.
 /*
 1.
 The US Gov has prohibited certain words (listed below) from being used in political party names. 
@@ -10,12 +11,14 @@ Hate
 War
 Kill
 */
+-- SM Tip: Add all colors to color table. It can be in data file.
 with x as(
 select PartyName  = 'Flower Power', YearBegan = 1970, Color = 'Violet'
 union select 'Love Powar', 1971, 'Green'
 union select 'Disco', 1980, 'White'
 union select 'Talent and Skill', 1990, 'Red'
 union select 'Rock and Roll', 1980, 'Pink'
+-- SM Should be null for color.
 union select 'Phosphate Pros', 2022, 'no color' 
 )
 insert party(ColorId, PartyName, YearBegan)
@@ -34,6 +37,7 @@ and x.partyname not like '%kill%'
 */
 ;
 with x as(
+-- SM Just do avg per party.
     select AverageAgeAtTermStart = avg(p.AgeAtTermStart), pt.partyname, p.lastname
     from president p 
     join party pt 
@@ -48,6 +52,7 @@ join president p
 on x.lastname = p.lastname 
 where p.AgeAtTermStart < x.AverageAgeAtTermStart 
 --3. Set the color of the party with the most presidents to Gold
+-- SM Tip: Should really update the id to gold.
 with x as(
     select top 1 NumPresidents = count(*), pt.PartyName
     from party pt 
@@ -65,6 +70,7 @@ join x
 on pt.PartyName = x.partyname 
 
 --4 Delete the executive orders of the party with the least presidential executive orders
+-- SM -50% This doesn't delete anything.
 with x as (
     select top 1 NumOrders = count(*), pt.PartyName
     from party pt 
