882fdb4c185cea6ada2dcc496abce72aa34ea5d9
Devorag <141979085+Devorag@users.noreply.github.com>
2024-03-27 00:24:17 -0400

    new stuff

--- software/desktop/LanguageBasics/LanguageBasics/frmLanguageBasics.Designer.cs
+++ software/desktop/LanguageBasics/LanguageBasics/frmLanguageBasics.Designer.cs
@@ -48,6 +48,18 @@
             btnNull = new Button();
             btnString = new Button();
             btnValueRefType = new Button();
+            btnScope1 = new Button();
+            btnSwitch = new Button();
+            btnTermary1 = new Button();
+            btnTermary2 = new Button();
+            btnFor1 = new Button();
+            btnFor2 = new Button();
+            btnForEach1 = new Button();
+            btnForEach2 = new Button();
+            btnWhile1 = new Button();
+            btnWhile2 = new Button();
+            btnTimer1 = new Button();
+            btnTimer2 = new Button();
             tblMain.SuspendLayout();
             tblOutput.SuspendLayout();
             SuspendLayout();
@@ -75,25 +87,40 @@
             tblMain.Controls.Add(btnNull, 2, 4);
             tblMain.Controls.Add(btnString, 3, 4);
             tblMain.Controls.Add(btnValueRefType, 0, 5);
+            tblMain.Controls.Add(btnScope1, 1, 5);
+            tblMain.Controls.Add(btnSwitch, 2, 5);
+            tblMain.Controls.Add(btnTermary1, 3, 5);
+            tblMain.Controls.Add(btnTermary2, 0, 6);
+            tblMain.Controls.Add(btnFor1, 1, 6);
+            tblMain.Controls.Add(btnFor2, 2, 6);
+            tblMain.Controls.Add(btnForEach1, 3, 6);
+            tblMain.Controls.Add(btnForEach2, 0, 7);
+            tblMain.Controls.Add(btnWhile1, 1, 7);
+            tblMain.Controls.Add(btnWhile2, 2, 7);
+            tblMain.Controls.Add(btnTimer1, 3, 7);
+            tblMain.Controls.Add(btnTimer2, 0, 8);
             tblMain.Dock = DockStyle.Fill;
             tblMain.Location = new Point(0, 0);
             tblMain.Name = "tblMain";
-            tblMain.RowCount = 6;
-            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 35.6435623F));
-            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 12.3762369F));
-            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 12.3762369F));
-            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 12.3762369F));
-            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 12.3762388F));
-            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 14.8514853F));
+            tblMain.RowCount = 9;
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 25.971571F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 9.017905F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 9.017905F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 9.017905F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 9.017907F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 10.8214874F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 9.020648F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 9.023762F));
+            tblMain.RowStyles.Add(new RowStyle(SizeType.Percent, 9.090908F));
             tblMain.Size = new Size(1304, 942);
             tblMain.TabIndex = 0;
             // 
             // btnEvemtHandler1
             // 
             btnEvemtHandler1.Dock = DockStyle.Fill;
-            btnEvemtHandler1.Location = new Point(4, 454);
+            btnEvemtHandler1.Location = new Point(4, 332);
             btnEvemtHandler1.Name = "btnEvemtHandler1";
-            btnEvemtHandler1.Size = new Size(318, 109);
+            btnEvemtHandler1.Size = new Size(318, 78);
             btnEvemtHandler1.TabIndex = 0;
             btnEvemtHandler1.Text = "Event Handler 1 ";
             btnEvemtHandler1.UseVisualStyleBackColor = true;
@@ -101,9 +128,9 @@
             // btnEventHandler2
             // 
             btnEventHandler2.Dock = DockStyle.Fill;
-            btnEventHandler2.Location = new Point(329, 454);
+            btnEventHandler2.Location = new Point(329, 332);
             btnEventHandler2.Name = "btnEventHandler2";
-            btnEventHandler2.Size = new Size(318, 109);
+            btnEventHandler2.Size = new Size(318, 78);
             btnEventHandler2.TabIndex = 1;
             btnEventHandler2.Text = "Event Handler 2 ";
             btnEventHandler2.UseVisualStyleBackColor = true;
@@ -111,9 +138,9 @@
             // btnVariable1
             // 
             btnVariable1.Dock = DockStyle.Fill;
-            btnVariable1.Location = new Point(654, 454);
+            btnVariable1.Location = new Point(654, 332);
             btnVariable1.Name = "btnVariable1";
-            btnVariable1.Size = new Size(318, 109);
+            btnVariable1.Size = new Size(318, 78);
             btnVariable1.TabIndex = 2;
             btnVariable1.Text = "Variable 1 ";
             btnVariable1.UseVisualStyleBackColor = true;
@@ -121,9 +148,9 @@
             // btnVariable2
             // 
             btnVariable2.Dock = DockStyle.Fill;
-            btnVariable2.Location = new Point(979, 454);
+            btnVariable2.Location = new Point(979, 332);
             btnVariable2.Name = "btnVariable2";
-            btnVariable2.Size = new Size(321, 109);
+            btnVariable2.Size = new Size(321, 78);
             btnVariable2.TabIndex = 3;
             btnVariable2.Text = "Variable 2 ";
             btnVariable2.UseVisualStyleBackColor = true;
@@ -141,11 +168,11 @@
             tblOutput.Controls.Add(btnRandom, 2, 0);
             tblOutput.Controls.Add(btnData, 3, 0);
             tblOutput.Dock = DockStyle.Fill;
-            tblOutput.Location = new Point(4, 338);
+            tblOutput.Location = new Point(4, 247);
             tblOutput.Name = "tblOutput";
             tblOutput.RowCount = 1;
             tblOutput.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tblOutput.Size = new Size(1296, 109);
+            tblOutput.Size = new Size(1296, 78);
             tblOutput.TabIndex = 5;
             // 
             // btnDataConversion1
@@ -153,7 +180,7 @@
             btnDataConversion1.Dock = DockStyle.Fill;
             btnDataConversion1.Location = new Point(3, 3);
             btnDataConversion1.Name = "btnDataConversion1";
-            btnDataConversion1.Size = new Size(318, 103);
+            btnDataConversion1.Size = new Size(318, 72);
             btnDataConversion1.TabIndex = 0;
             btnDataConversion1.Text = "Data Conversion 1";
             btnDataConversion1.UseVisualStyleBackColor = true;
@@ -163,7 +190,7 @@
             btnDataConversion2.Dock = DockStyle.Fill;
             btnDataConversion2.Location = new Point(327, 3);
             btnDataConversion2.Name = "btnDataConversion2";
-            btnDataConversion2.Size = new Size(318, 103);
+            btnDataConversion2.Size = new Size(318, 72);
             btnDataConversion2.TabIndex = 1;
             btnDataConversion2.Text = "Data Conversion 2";
             btnDataConversion2.UseVisualStyleBackColor = true;
@@ -173,7 +200,7 @@
             btnRandom.Dock = DockStyle.Fill;
             btnRandom.Location = new Point(651, 3);
             btnRandom.Name = "btnRandom";
-            btnRandom.Size = new Size(318, 103);
+            btnRandom.Size = new Size(318, 72);
             btnRandom.TabIndex = 2;
             btnRandom.Text = "Random ";
             btnRandom.UseVisualStyleBackColor = true;
@@ -183,7 +210,7 @@
             btnData.Dock = DockStyle.Fill;
             btnData.Location = new Point(975, 3);
             btnData.Name = "btnData";
-            btnData.Size = new Size(318, 103);
+            btnData.Size = new Size(318, 72);
             btnData.TabIndex = 3;
             btnData.Text = "Show Data in Grid";
             btnData.UseVisualStyleBackColor = true;
@@ -196,15 +223,15 @@
             txtOutput.Multiline = true;
             txtOutput.Name = "txtOutput";
             txtOutput.ScrollBars = ScrollBars.Both;
-            txtOutput.Size = new Size(318, 327);
+            txtOutput.Size = new Size(318, 236);
             txtOutput.TabIndex = 4;
             // 
             // btnIf1
             // 
             btnIf1.Dock = DockStyle.Fill;
-            btnIf1.Location = new Point(4, 570);
+            btnIf1.Location = new Point(4, 417);
             btnIf1.Name = "btnIf1";
-            btnIf1.Size = new Size(318, 109);
+            btnIf1.Size = new Size(318, 78);
             btnIf1.TabIndex = 6;
             btnIf1.Text = "If 1 ";
             btnIf1.UseVisualStyleBackColor = true;
@@ -212,9 +239,9 @@
             // btnIf2
             // 
             btnIf2.Dock = DockStyle.Fill;
-            btnIf2.Location = new Point(329, 570);
+            btnIf2.Location = new Point(329, 417);
             btnIf2.Name = "btnIf2";
-            btnIf2.Size = new Size(318, 109);
+            btnIf2.Size = new Size(318, 78);
             btnIf2.TabIndex = 7;
             btnIf2.Text = "If 2";
             btnIf2.UseVisualStyleBackColor = true;
@@ -222,9 +249,9 @@
             // btnAddControl1
             // 
             btnAddControl1.Dock = DockStyle.Fill;
-            btnAddControl1.Location = new Point(654, 570);
+            btnAddControl1.Location = new Point(654, 417);
             btnAddControl1.Name = "btnAddControl1";
-            btnAddControl1.Size = new Size(318, 109);
+            btnAddControl1.Size = new Size(318, 78);
             btnAddControl1.TabIndex = 8;
             btnAddControl1.Text = "Add Control 1 ";
             btnAddControl1.UseVisualStyleBackColor = true;
@@ -232,9 +259,9 @@
             // btnAddControl2
             // 
             btnAddControl2.Dock = DockStyle.Fill;
-            btnAddControl2.Location = new Point(979, 570);
+            btnAddControl2.Location = new Point(979, 417);
             btnAddControl2.Name = "btnAddControl2";
-            btnAddControl2.Size = new Size(321, 109);
+            btnAddControl2.Size = new Size(321, 78);
             btnAddControl2.TabIndex = 9;
             btnAddControl2.Text = "Add Control 2 ";
             btnAddControl2.UseVisualStyleBackColor = true;
@@ -242,9 +269,9 @@
             // btnObject
             // 
             btnObject.Dock = DockStyle.Fill;
-            btnObject.Location = new Point(4, 686);
+            btnObject.Location = new Point(4, 502);
             btnObject.Name = "btnObject";
-            btnObject.Size = new Size(318, 109);
+            btnObject.Size = new Size(318, 78);
             btnObject.TabIndex = 10;
             btnObject.Text = "Object data type";
             btnObject.UseVisualStyleBackColor = true;
@@ -252,9 +279,9 @@
             // btnNew
             // 
             btnNew.Dock = DockStyle.Fill;
-            btnNew.Location = new Point(329, 686);
+            btnNew.Location = new Point(329, 502);
             btnNew.Name = "btnNew";
-            btnNew.Size = new Size(318, 109);
+            btnNew.Size = new Size(318, 78);
             btnNew.TabIndex = 11;
             btnNew.Text = "instatiating with New";
             btnNew.UseVisualStyleBackColor = true;
@@ -262,9 +289,9 @@
             // btnNull
             // 
             btnNull.Dock = DockStyle.Fill;
-            btnNull.Location = new Point(654, 686);
+            btnNull.Location = new Point(654, 502);
             btnNull.Name = "btnNull";
-            btnNull.Size = new Size(318, 109);
+            btnNull.Size = new Size(318, 78);
             btnNull.TabIndex = 12;
             btnNull.Text = "working with Null";
             btnNull.UseVisualStyleBackColor = true;
@@ -272,9 +299,9 @@
             // btnString
             // 
             btnString.Dock = DockStyle.Fill;
-            btnString.Location = new Point(979, 686);
+            btnString.Location = new Point(979, 502);
             btnString.Name = "btnString";
-            btnString.Size = new Size(321, 109);
+            btnString.Size = new Size(321, 78);
             btnString.TabIndex = 13;
             btnString.Text = "working with string";
             btnString.UseVisualStyleBackColor = true;
@@ -282,12 +309,133 @@
             // btnValueRefType
             // 
             btnValueRefType.Dock = DockStyle.Fill;
-            btnValueRefType.Location = new Point(4, 802);
+            btnValueRefType.Location = new Point(4, 587);
             btnValueRefType.Name = "btnValueRefType";
-            btnValueRefType.Size = new Size(318, 136);
+            btnValueRefType.Size = new Size(318, 94);
             btnValueRefType.TabIndex = 14;
             btnValueRefType.Text = "working with value and ref types ";
             btnValueRefType.UseVisualStyleBackColor = true;
+            // 
+            // btnScope1
+            // 
+            btnScope1.Dock = DockStyle.Fill;
+            btnScope1.Location = new Point(329, 587);
+            btnScope1.Name = "btnScope1";
+            btnScope1.Size = new Size(318, 94);
+            btnScope1.TabIndex = 15;
+            btnScope1.Text = "Scope 1 ";
+            btnScope1.UseVisualStyleBackColor = true;
+            // 
+            // btnSwitch
+            // 
+            btnSwitch.Dock = DockStyle.Fill;
+            btnSwitch.Location = new Point(654, 587);
+            btnSwitch.Name = "btnSwitch";
+            btnSwitch.Size = new Size(318, 94);
+            btnSwitch.TabIndex = 16;
+            btnSwitch.Text = "Switch";
+            btnSwitch.UseVisualStyleBackColor = true;
+            // 
+            // btnTermary1
+            // 
+            btnTermary1.Dock = DockStyle.Fill;
+            btnTermary1.Location = new Point(979, 587);
+            btnTermary1.Name = "btnTermary1";
+            btnTermary1.Size = new Size(321, 94);
+            btnTermary1.TabIndex = 17;
+            btnTermary1.Text = "?: 1";
+            btnTermary1.UseVisualStyleBackColor = true;
+            // 
+            // btnTermary2
+            // 
+            btnTermary2.Dock = DockStyle.Fill;
+            btnTermary2.Location = new Point(4, 688);
+            btnTermary2.Name = "btnTermary2";
+            btnTermary2.Size = new Size(318, 78);
+            btnTermary2.TabIndex = 18;
+            btnTermary2.Text = "?: 2";
+            btnTermary2.UseVisualStyleBackColor = true;
+            // 
+            // btnFor1
+            // 
+            btnFor1.Dock = DockStyle.Fill;
+            btnFor1.Location = new Point(329, 688);
+            btnFor1.Name = "btnFor1";
+            btnFor1.Size = new Size(318, 78);
+            btnFor1.TabIndex = 19;
+            btnFor1.Text = "For Loop 1 ";
+            btnFor1.UseVisualStyleBackColor = true;
+            btnFor1.Click += button1_Click;
+            // 
+            // btnFor2
+            // 
+            btnFor2.Dock = DockStyle.Fill;
+            btnFor2.Location = new Point(654, 688);
+            btnFor2.Name = "btnFor2";
+            btnFor2.Size = new Size(318, 78);
+            btnFor2.TabIndex = 20;
+            btnFor2.Text = "For Loop 2 ";
+            btnFor2.UseVisualStyleBackColor = true;
+            // 
+            // btnForEach1
+            // 
+            btnForEach1.Dock = DockStyle.Fill;
+            btnForEach1.Location = new Point(979, 688);
+            btnForEach1.Name = "btnForEach1";
+            btnForEach1.Size = new Size(321, 78);
+            btnForEach1.TabIndex = 21;
+            btnForEach1.Text = "For Each 1 ";
+            btnForEach1.UseVisualStyleBackColor = true;
+            // 
+            // btnForEach2
+            // 
+            btnForEach2.Dock = DockStyle.Fill;
+            btnForEach2.Location = new Point(4, 773);
+            btnForEach2.Name = "btnForEach2";
+            btnForEach2.Size = new Size(318, 78);
+            btnForEach2.TabIndex = 22;
+            btnForEach2.Text = "For Each 2 ";
+            btnForEach2.UseVisualStyleBackColor = true;
+            // 
+            // btnWhile1
+            // 
+            btnWhile1.Dock = DockStyle.Fill;
+            btnWhile1.Location = new Point(329, 773);
+            btnWhile1.Name = "btnWhile1";
+            btnWhile1.Size = new Size(318, 78);
+            btnWhile1.TabIndex = 23;
+            btnWhile1.Text = "While 1 ";
+            btnWhile1.UseVisualStyleBackColor = true;
+            // 
+            // btnWhile2
+            // 
+            btnWhile2.Dock = DockStyle.Fill;
+            btnWhile2.Location = new Point(654, 773);
+            btnWhile2.Name = "btnWhile2";
+            btnWhile2.Size = new Size(318, 78);
+            btnWhile2.TabIndex = 24;
+            btnWhile2.Text = "While 2 ";
+            btnWhile2.UseVisualStyleBackColor = true;
+            // 
+            // btnTimer1
+            // 
+            btnTimer1.Dock = DockStyle.Fill;
+            btnTimer1.Location = new Point(979, 773);
+            btnTimer1.Name = "btnTimer1";
+            btnTimer1.Size = new Size(321, 78);
+            btnTimer1.TabIndex = 25;
+            btnTimer1.Text = "Timer 1 ";
+            btnTimer1.UseVisualStyleBackColor = true;
+            // 
+            // btnTimer2
+            // 
+            btnTimer2.Dock = DockStyle.Fill;
+            btnTimer2.Location = new Point(4, 858);
+            btnTimer2.Name = "btnTimer2";
+            btnTimer2.Size = new Size(318, 80);
+            btnTimer2.TabIndex = 26;
+            btnTimer2.Text = "Timer 2 ";
+            btnTimer2.UseVisualStyleBackColor = true;
             // 
             // frmLanguageBasics
             // 
@@ -327,5 +475,17 @@
         private Button btnNull;
         private Button btnString;
         private Button btnValueRefType;
+        private Button btnScope1;
+        private Button btnSwitch;
+        private Button btnTermary1;
+        private Button btnTermary2;
+        private Button btnFor1;
+        private Button btnFor2;
+        private Button btnForEach1;
+        private Button btnForEach2;
+        private Button btnWhile1;
+        private Button btnWhile2;
+        private Button btnTimer1;
+        private Button btnTimer2;
     }
 }
