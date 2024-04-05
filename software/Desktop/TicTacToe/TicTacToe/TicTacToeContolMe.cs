1ba2d04d4cec7bcffb612be1d9498f7d427f1454
Devorag <141979085+Devorag@users.noreply.github.com>
2024-04-05 01:20:42 -0400

    block user from clicking on buton that's already taken

--- software/Desktop/TicTacToe/TicTacToe/TicTacToeContolMe.cs
+++ software/Desktop/TicTacToe/TicTacToe/TicTacToeContolMe.cs
@@ -12,10 +12,45 @@ namespace TicTacToe
 {
     public partial class TicTacToeContolMe : TicTacToeControls
     {
+        enum TurnEnum { X, O };
+        TurnEnum currentturn = TurnEnum.X;
+
+        List<Button> lstbuttons;
         public TicTacToeContolMe()
         {
             InitializeComponent();
             lblName.Text = "Me";
+            lstbuttons = new() { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9 };
+            lstbuttons.ForEach(b => b.Click += SpotButton_Click);
+        }
+
+
+
+        private void DoTurn(Button btn)
+        {
+            if (btn.Text == " ")
+            {
+                btn.Text = currentturn.ToString();
+
+                if (currentturn == TurnEnum.X)
+                {
+                    currentturn = TurnEnum.O;
+                }
+                else
+                {
+                    currentturn = TurnEnum.X;
+                }
+            }
+        }
+
+        private void SpotButton_Click(object? sender, EventArgs e)
+        {
+            if (sender is Button)
+            {
+                DoTurn((Button)sender);
+            }
         }
     }
+
+
 }
