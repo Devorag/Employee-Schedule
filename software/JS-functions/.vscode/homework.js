3445d795b8741fd7cde90d847929abf57070d424
AvigailGellis <agellis@cpucodeschool.com>
2024-09-02 10:57:37 +0300

    #76

--- software/JS-functions/.vscode/homework.js
+++ software/JS-functions/.vscode/homework.js
@@ -13,7 +13,9 @@ function Description(f) {
   }
 
 // Example 1 : 
+//AS Instructions said that function names should be multiple words joined by underscores, fix the naming for all 3.
 function discountBasedOnName(price) {
+//AS Tip: It would be nice to convert all of this into one line of code.
     const itemnameLowerCase = itemname.toLowerCase();
     const hasAorE = itemnameLowerCase.includes('a') || itemnameLowerCase.includes('e');
     return (hasAorE && price * .9) || price * .8
@@ -21,13 +23,14 @@ function discountBasedOnName(price) {
 }
 
 const randomSurcharge = function(price) {
+//AS Same as above comment
     const randomValue = Math.floor(Math.random() * 100);
     const result = price + randomValue;
     return result;
 }
 // Example 3 : 
 const fiftyPercentDiscount = price => price * .5;
-
+//AS Take out the variable itemname and put toy directly into the function like the others.
 let itemname = "Toy";
 calculatePrice(itemname, 100, discountBasedOnName);
 console.log(b);
