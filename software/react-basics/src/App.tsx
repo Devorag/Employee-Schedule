780d730bc00e21368753ddc3fd39e8ef85986f0a
Devorag <141979085+Devorag@users.noreply.github.com>
2024-10-14 11:02:28 -0400

    following 19 vids

--- software/react-basics/src/App.tsx
+++ software/react-basics/src/App.tsx
@@ -3,10 +3,43 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import PresidentSummary from "./PresidentSummary"
 import PresidentList from "./PresidentList"
 import PresidentFeature from "./PresidentFeature"
-import PresidentData from "./PresidentData.json"
+import initalPresidents from "./PresidentData.json"
+import { useState } from "react"
 
 function App() {
+  const [presidents, setPresidents] = useState(initalPresidents);
+  const [featured, setFeatured] = useState(0);
+  const getCopyOfPresidents = () => presidents.map(p => ({ ...p }));
 
+  const handleChangePrez = () => {
+    const newPresidents = getCopyOfPresidents();
+    if (newPresidents.length > 0) {
+
+      newPresidents[0].lastName = newPresidents[0].lastName + "A";
+      console.log("current", presidents[0].lastName);
+      console.log("new", newPresidents[0].lastName);
+      setPresidents(newPresidents);
+    }
+  };
+
+  const handleAddPresident = () => {
+    const newPresidents = getCopyOfPresidents();
+    if (newPresidents.length > 0) {
+      const newPrez = { ...newPresidents[newPresidents.length - 1] };
+      newPrez.lastName += "B";
+      newPrez.num++;
+      newPresidents.push(newPrez);
+      setPresidents(newPresidents);
+    }
+  };
+
+  const handleChangeFeatured = (value: number) => {
+    let newFeatured = featured;
+    newFeatured++;
+    if (newFeatured > -1 && newFeatured <= presidents.length - 1) {
+      setFeatured(newFeatured);
+    }
+  }
 
   return (
     <div className="container">
@@ -20,24 +53,36 @@ function App() {
         <div className="col-6">
           <div className="row">
             <div className="col-12">
-              <PresidentSummary presidents={PresidentData} />
+              <PresidentSummary presidents={presidents} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-3">
+              <button onClick={handleChangePrez} className="btn btn-primary">Change prez</button>
+            </div>
+            <div className="col-3">
+              <button onClick={handleAddPresident} className="btn btn-primary">Add prez</button>
             </div>
           </div>
           <div className="row">
             <div className="col-12">
-              <  PresidentList presidents={PresidentData} />
+              <  PresidentList presidents={presidents} />
             </div>
           </div>
         </div>
         <div className="col-6">
           <div className="row">
+            <div className="col-4"><button disabled={featured == 0} onClick={() => handleChangeFeatured(-1)} className="btn btn-success">Back</button></div>
+            <div className="col-4"><button disabled={featured == presidents.length - 1} onClick={() => handleChangeFeatured(1)} className="btn btn-success">Next {featured}</button></div>
+          </div>
+          <div className="row">
             <div className="col-12">
-              <PresidentFeature president={PresidentData[0]} />
+              <PresidentFeature president={presidents[featured]} />
             </div>
           </div>
           <div className="row">
             <div className="col-12">
-              <PresidentFeature president={PresidentData[2]} />
+              <PresidentFeature president={presidents[2]} />
             </div>
           </div>
         </div>
