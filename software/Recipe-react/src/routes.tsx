d1eda3ef017aa338002f2fab3b05f39439729ddf
Devorag <141979085+Devorag@users.noreply.github.com>
2024-12-21 20:19:43 -0500

    #94

--- software/Recipe-react/src/routes.tsx
+++ software/Recipe-react/src/routes.tsx
@@ -6,14 +6,18 @@ import Cookbooks from "./Cookbooks";
 import Meals from "./Meals";
 import Login from "./Login";
 import ProtectedRoute from "./ProtectedRoute";
+import { RecipeEdit } from "./RecipeEdit";
 
 
 const router = createBrowserRouter([
     {
-        path: "/", element: <Recipes />, children: [
-            { index: true, element: <Navbar /> },
-        ]
+        path: "/",
+        element: <Navbar />,
+        children: [
+            { index: true, element: <Recipes /> },
+        ],
     },
+    { path: "/edit", element: <RecipeEdit /> },
     { path: "meals", element: <ProtectedRoute element={<Meals />} requiredrole={0} /> },
     { path: "cookbooks", element: <ProtectedRoute element={<Cookbooks />} requiredrole={0} /> },
     { path: "login", element: <Login fromPath={location.pathname} /> }
